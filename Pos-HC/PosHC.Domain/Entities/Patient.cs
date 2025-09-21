b03e8df1c06145b4697e6313eea594bcfb5a031f
ali.ballouk <aliballouk@DESKTOP-5KQU0H8>
2025-09-21 14:38:25 +0300

    adding Patient and VisitItem in Domain

---
+++ Pos-HC/PosHC.Domain/Entities/Patient.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PosHC.Domain.Entities
+{
+    [Table("Patient", Schema = "poshc")]
+    public class Patient
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; } 
+        public string LastName { get; set; }
+    }
+}
---
+++ Pos-HC/PosHC.Domain/Entities/VisitItem.cs
@@ -0,0 +1,56 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json;
+
+namespace PosHC.Domain.Entities
+{
+    [Table("VisitItem", Schema = "poshc")]
+    public abstract class VisitItem
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = null!;
+        public decimal UnitPrice { get; set; }
+
+        public string SettingsJson { get; set; } = "{}";
+
+        public abstract object Settings { get; set; }
+
+        public void SaveSettings(object settings)
+        {
+            SettingsJson = JsonSerializer.Serialize(settings);
+        }
+
+        protected T DeserializeSettings<T>()
+        {
+            return JsonSerializer.Deserialize<T>(SettingsJson)!;
+        }
+    }
+
+    public class ProductItem : VisitItem
+    {
+        public override object Settings
+        {
+            get => DeserializeSettings<ProductSettings>();
+            set => SaveSettings(value);
+        }
+    }
+
+    public class ServiceItem : VisitItem
+    {
+        public override object Settings
+        {
+            get => DeserializeSettings<ServiceSettings>();
+            set => SaveSettings(value);
+        }
+    }
+
+    public class ProductSettings
+    {
+        public string Name { get; set; } = "";
+    }
+
+    public class ServiceSettings
+    {
+        public string Duration { get; set; } = "";
+        public string Specialty { get; set; } = "";
+    }
+}
